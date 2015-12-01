e739c5cb8b401513d60a3d5dfc55ee51939d7e80
Dianne Buenaobra <batiyhan19@gmail.com>
2015-12-01 04:26:42 -0500

    Initialized commit

---
+++ save22_productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from save22_productspiders.items import Save22ProductspidersItem
+
+
+class WwwExpansysComSgCrawler(CrawlSpider):
+  name = 'www_expansys_com_sg_crawler'
+  allowed_domains = ['expansys.com.sg']
+  start_urls = [
+      'http://www.epansys.com.sg/mobile-phones/'
+      ]
+  
+  rules = (
+    Rule(LinkExtractor(
+        allow = (r'(.+)',),
+        callback = 'parse_item',
+    )),
+  )
+  
+  def parse_item(self,response):
+    pass
