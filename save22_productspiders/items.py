9693030fb9807d1cfe930b3bcec0724fca6a2ee9
Dianne Buenaobra <batiyhan19@gmail.com>
2015-12-01 04:06:49 -0500

    Added new fields for the product item spider.

--- save22_productspiders/items.py
+++ save22_productspiders/items.py
@@ -11,4 +11,22 @@ import scrapy
 class Save22ProductspidersItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
+    retailer_sku_code = scrapy.Field()
+    model = scrapy.Field()
+    mpn = scrapy.Field()
+    sku = scrapy.Field()
+    upc = scrapy.Field()
+    ean = scrapy.Field()
+    currency = scrapy.Field()
+    price = scrapy.Field()
+    crawl_time = scrapy.Field()
+    promo_price = scrapy.Field()
+    promo_qty = scrapy.Field()
+    promo_data = scrapy.Field()
+    
+    
     pass
+    
