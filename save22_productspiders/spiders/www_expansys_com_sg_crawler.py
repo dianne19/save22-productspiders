da2c16370a2c202b3f50f3136c95a982db291366
Dianne Buenaobra <batiyhan19@gmail.com>
2015-12-03 03:48:38 -0500

    Initial commit

--- save22_productspiders/spiders/www_expansys_com_sg_crawler.py
+++ save22_productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -1,47 +1,25 @@
-from datetime import datetime
-
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-
+import scrapy
 from save22_productspiders.items import Save22ProductspidersItem
 
+class ExpansysItem(scrapy.Spider):
+    name = "expansys"
+    allowed_domains = ["expansys.com.sg"]
+    start_urls = [
+        "http://www.expansys.com.sg/mobile-phones/",
+    ]
 
-class WwwExpansysComSgCrawler(CrawlSpider):
-  name = 'www_expansys_com_sg_crawler'
-  allowed_domains = ['expansys.com.sg']
-  start_urls = [
-      'http://www.epansys.com.sg/mobile-phones/'
-      ]
-  
-  rules = (
-    Rule(
-      LinkExtractor(
-        allow = (r'(.+)',),
-        ),
-        callback = 'parse_item',
-    ),
-  )
-  
-      
-def parse(self, response):
-  for href in response.css("div.span2.categorybox-span > div.categorybox.thumbnail.text-center > div.thumb > a::attr('href')"):
-    url = response.urljoin(href.extract())
-    print url
-    request = scrapy.Request(url, callback=self.parse_2)
-    yield request
-
-def parse_2(self, response):
-    for href in response.css("div.FeatueredHeader > h2 > a::attr('href')"):
-    url = response.urljoin(href.extract())
-    request = scrapy.Request(url, callback=self.parse_dir_contents)
-    yield request
+    def parse(self, response):
+        for href in response.css("div#nav > ul.asia.me > li > a::attr('href')"):   
+          url = response.urljoin(href.extract())
+          request = scrapy.Request(url, callback=self.headd)
+          yield request
 
 
-def parse_dir_contents(self, response):
-    for sel in response.xpath('//div#prod-data-id126641.prod-data'):
-    item = AllforYouItem()
-    item['name'] = sel.xpath('text()').extract()
-    item['title'] = response.xpath = ('//*[id="product"]/*[@id="title"]/h3/text()'.extract()
-    item['link'] = sel.xpath('a/@href').extract()
-    item['desc'] = sel.xpath('text()').extract()
-    yield item
+    def headd(self, response):
+        for href in response.css("div#product_listing > div.productGrid > ul.item.c0 > li.image > a::attr('href')"):
+          url = response.urljoin(href.extract())
+          request = scrapy.Request(url, callback=self.parse_dir_contents)
+          yield request      
+
+    def parse_dir_contents(self, response):
+        items = list()
