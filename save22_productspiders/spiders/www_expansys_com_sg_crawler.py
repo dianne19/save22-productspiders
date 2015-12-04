a3cbb25dfb8cfe72e92041604b33b2b3df8c53ad
Dianne Buenaobra <batiyhan19@gmail.com>
2015-12-04 00:52:35 -0500

    Initial commit

--- save22_productspiders/spiders/www_expansys_com_sg_crawler.py
+++ save22_productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -11,15 +11,29 @@ class ExpansysItem(scrapy.Spider):
     def parse(self, response):
         for href in response.css("div#nav > ul.asia.me > li > a::attr('href')"):   
           url = response.urljoin(href.extract())
-          request = scrapy.Request(url, callback=self.headd)
+          request = scrapy.Request(url, callback=self.parse_2)
           yield request
 
 
-    def headd(self, response):
+    def parse_2(self, response):
         for href in response.css("div#product_listing > div.productGrid > ul.item.c0 > li.image > a::attr('href')"):
           url = response.urljoin(href.extract())
           request = scrapy.Request(url, callback=self.parse_dir_contents)
           yield request      
 
-    def parse_dir_contents(self, response):
-        items = list()
+    def parse_dir_contents(self, response): 
+      items = list()
+
+          for sel in response.xpath('//div[@class="prod_head"]'):
+            item = Save22ProductspidersItem()
+            
+              item['Link'] = response.url or None
+          #    item['Title'] = sel.xpath('@data-name').extract()
+          #    item['Description'] = sel.xpath('@data-desc').extract()
+           #   item['Image_Link'] = sel.xpath('@data-imgurl').extract()
+          #    item['Quality'] = sel.xpath('@data-selqty').extract()
+           #   item['Price'] = sel.xpath('@data-price').extract()
+            #  item['Old_price'] = sel.xpath('@data-oldprice').extract()
+             # item['Out_of_stock'] = sel.xpath('@data-outofstock').extract()
+            #  items.append(item)
+             # yield item
